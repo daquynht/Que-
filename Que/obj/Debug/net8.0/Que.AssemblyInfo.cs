5907c16a84d24be3c90803b35c3e9a10f59585ce
s369519 <s369519@oslomet.no>
2025-10-10 10:43:24 +0200

    s

--- Que/obj/Debug/net8.0/Que.AssemblyInfo.cs
+++ Que/obj/Debug/net8.0/Que.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Que")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88446cb1d51d32b48b8a5e9f92180d42e8887291")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d03de336a7e69b54c665041b9d22943cc65d7cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Que")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Que")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
